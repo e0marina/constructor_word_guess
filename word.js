74ecc77ddf767a3d5e2d5a01ba1dc55832027587
Emily M <30880308+e0marina@users.noreply.github.com>
2020-03-08 11:36:32 -0700

    used prototype and tostring() to create a string out of the new obj

--- word.js
+++ word.js
@@ -10,7 +10,7 @@
 //requiring Letter function from the other doc
 var Letter = require("./letter");
 //var to hold one of the possible words the user could guess
-var randomWord = "pear";
+var randomWord = "guava";
 //var to hold array of letter objects
 var letterArr = [];
 
@@ -18,28 +18,29 @@ var letterArr = [];
 function Word() {
   //maybe some kind of if statement that "decides" which letter array is selected
   if (randomWord === "apple") {
-    var letter1 = new Letter("a", false);
-    var letter2 = new Letter("p", false);
-    var letter3 = new Letter("p", false);
-    var letter4 = new Letter("l", false);
-    var letter5 = new Letter("e", false);
-  } else if (randomWord === "pear") {
-    var letter1 = new Letter("p", false);
-    var letter2 = new Letter("e", false);
-    var letter3 = new Letter("a", false);
-    var letter4 = new Letter("r", false);
+    var letter1 = new Letter("a");
+    var letter2 = new Letter("p");
+    var letter3 = new Letter("p");
+    var letter4 = new Letter("l");
+    var letter5 = new Letter("e");
+  } else if (randomWord === "guava") {
+    var letter1 = new Letter("g");
+    var letter2 = new Letter("u");
+    var letter3 = new Letter("a");
+    var letter4 = new Letter("v");
+    var letter5 = new Letter("a");
   } else if (randomWord === "grape") {
-    var letter1 = new Letter("g", false);
-    var letter2 = new Letter("r", false);
-    var letter3 = new Letter("a", false);
-    var letter4 = new Letter("p", false);
-    var letter5 = new Letter("e", false);
+    var letter1 = new Letter("g");
+    var letter2 = new Letter("r");
+    var letter3 = new Letter("a");
+    var letter4 = new Letter("p");
+    var letter5 = new Letter("e");
   } else if (randomWord === "peach") {
-    var letter1 = new Letter("p", false);
-    var letter2 = new Letter("e", false);
-    var letter3 = new Letter("a", false);
-    var letter4 = new Letter("c", false);
-    var letter5 = new Letter("h", false);
+    var letter1 = new Letter("p");
+    var letter2 = new Letter("e");
+    var letter3 = new Letter("a");
+    var letter4 = new Letter("c");
+    var letter5 = new Letter("h");
   }
 
   //push Letter objects into the letterArr (array of obj)
@@ -47,10 +48,15 @@ function Word() {
   console.log(letterArr);
 
   // A function that returns a string representing the word. This should call the function on each letter object (the first function defined in Letter.js) that displays the character or an underscore and concatenate those together.
-  function stringTheWord() {
-    console.log(letterArr);
-    //look at prototype and to string
-  }
-  stringTheWord();
+  Letter.prototype.toString = function stringTheWord() {
+    return "" + this.underChar;
+  };
+  console.log(
+    letter1.toString(),
+    letter2.toString(),
+    letter3.toString(),
+    letter4.toString(),
+    letter5.toString()
+  );
 }
 Word();
